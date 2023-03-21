c7af6d4bdf82e7ca77ec84b55c3dab80a3197a09
amitkedmi <amitkedmi98@gmail.com>
2023-03-21 12:02:28 +0200

    Update the table

--- lineup.js
+++ lineup.js
@@ -1,26 +1,96 @@
 const players = [
+    { name: '--choose--', position: 'forward' },
     { name: 'Ronaldo', position: 'forward' },
     { name: 'Messi', position: 'forward' },
     { name: 'Neymar', position: 'forward' },
+    { name: 'Mbappe', position: 'forward' },
+    { name: 'Haaland', position: 'forward' },
+    { name: 'Vinicius', position: 'forward' },
+    { name: '--choose--', position: 'midfielder' },
     { name: 'Modric', position: 'midfielder' },
     { name: 'Kroos', position: 'midfielder' },
-    { name: 'De Bruyne', position: 'midfielder' },
+    { name: 'De Bruyne', position: 'midfielder'},
+    { name: 'Pedri', position: 'midfielder'},
+    { name: 'Gavi', position: 'midfielder'},
+    { name: 'Pogba', position: 'midfielder'},
+    { name: '--choose--', position: 'defender' },
     { name: 'Alaba', position: 'defender' },
     { name: 'Ramos', position: 'defender' },
     { name: 'Varane', position: 'defender' },
+    { name: 'Araujo', position: 'defender' },
+    { name: 'Canselo', position: 'defender' },
+    { name: '--choose--', position: 'goalkeeper' },    
     { name: 'Neuer', position: 'goalkeeper' },
     { name: 'Buffon', position: 'goalkeeper' },
-    { name: 'Casillas', position: 'goalkeeper' }
+    { name: 'Casillas', position: 'goalkeeper' },
+    { name: 'Valdes', position: 'goalkeeper' }
 ]
 /*filter all the goalkeeper from players array */
-const goalkeeper = players.filter(function(player){
-    return player.position == "goalkeeper";   
+//goalkeeper:
+const goalkeeper = players.filter(function (player) {
+    return player.position == "goalkeeper";
 })
 
-const mappedGoalkeepers = goalkeeper.map(function(item){
+const mappedGoalkeepers = goalkeeper.map(function (item) {
     return `<option value="${item.name}">${item.name}</option>`
 });
 
 document.getElementById('pos1').innerHTML = mappedGoalkeepers.join('');
+
+//defender:
+const defender = players.filter(function (player) {
+    return player.position == "defender";
+})
+
+const mappeddefender = defender.map(function (item) {
+    return `<option value="${item.name}">${item.name}</option>`
+});
+
+document.getElementById('def1').innerHTML = mappeddefender.join('');
+document.getElementById('def2').innerHTML = mappeddefender.join('');
+document.getElementById('def3').innerHTML = mappeddefender.join('');
+document.getElementById('def4').innerHTML = mappeddefender.join('');
+document.getElementById('def5').innerHTML = mappeddefender.join('');
+
+//midfielder:
+const midfielder = players.filter(function (player) {
+    return player.position == "midfielder";
+})
+
+const mappeddmidfielder = midfielder.map(function (item) {
+    return `<option value="${item.name}">${item.name}</option>`
+});
+
+document.getElementById('mid1').innerHTML = mappeddmidfielder.join('');
+document.getElementById('mid2').innerHTML = mappeddmidfielder.join('');
+document.getElementById('mid3').innerHTML = mappeddmidfielder.join('');
+document.getElementById('mid4').innerHTML = mappeddmidfielder.join('');
+document.getElementById('mid5').innerHTML = mappeddmidfielder.join('');
+
+//forward:
+const forward = players.filter(function (player) {
+    return player.position == "forward";
+})
+
+const mappeddforward = forward.map(function (item) {
+    return `<option value="${item.name}">${item.name}</option>`
+});
+
+document.getElementById('st1').innerHTML = mappeddforward.join('');
+document.getElementById('st2').innerHTML = mappeddforward.join('');
+document.getElementById('st3').innerHTML = mappeddforward.join('');
+
+
+const btn = document.getElementById('sum');
+function lineUpConsole() {
+    console.log(" " + document.getElementById("pos1").value) +
+    console.log(" " + document.getElementById("def1").value + " " + document.getElementById("def2").value + " " + document.getElementById("def3").value + " " + document.getElementById("def4").value + " " + document.getElementById("def5").value)
+    console.log(" " + document.getElementById("mid1").value + " " + document.getElementById("mid2").value + " " + document.getElementById("mid3").value + " " + document.getElementById("mid4").value + " " + document.getElementById("mid5").value)
+    console.log(" " + document.getElementById("st1").value + " " + document.getElementById("st2").value + " " + document.getElementById("st3").value)
+}
+btn.addEventListener('click', lineUpConsole);
+
+
+
 
 
